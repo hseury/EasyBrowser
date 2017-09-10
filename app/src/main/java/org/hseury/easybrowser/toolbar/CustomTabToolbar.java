844d4665ae811caa916de92f1e1ad1b8bf9b092b
hseury <hseury@gmail.com>
2017-09-10 12:13:50 +0800

    refact layout

---
+++ app/src/main/java/org/hseury/easybrowser/toolbar/CustomTabToolbar.java
@@ -0,0 +1,14 @@
+package org.hseury.easybrowser.toolbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by hseury on 9/9/17.
+ */
+
+public class CustomTabToolbar extends ToolBarLayout {
+  public CustomTabToolbar(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+}
